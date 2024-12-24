3c7e7a29c165ea2addfc3e25eaa063cd99f630a3
Vortex-Vortex <paulo.g.mamani@gmail.com>
2024-12-24 19:00:46 -0300

    Fix table property definition and naughty beautiful configs

--- module/notifications.lua
+++ module/notifications.lua
@@ -15,14 +15,16 @@ config = {
     font = beautiful.system_font .. '8',
     icon = nil,
     icon_size = 50,
+    bg = beautiful.bg_color,
+    fg = beautiful.fg_color,
     shape = gears.shape.rectangle,
     border_width = beautiful.border_width,
-    border_color = beautiful.border_color,
+    border_color = beautiful.border_focus,
     hover_timeout = 0
 }
 
 for property, value in pairs(config) do
-    naughty.config.defaults.property = value
+    naughty.config.defaults[property] = value
 end
 
     -- Error Handling
