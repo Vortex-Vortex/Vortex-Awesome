77531a462a2776c7a7a9a2b4c438d06a7557787f
Vortex-Vortex <paulo.g.mamani@gmail.com>
2024-12-24 22:56:37 -0300

    Add button widget creator

---
+++ widget/material/button-widget.lua
@@ -0,0 +1,19 @@
+local gears = require('gears')
+local wibox = require('wibox')
+
+local icons = require('theme.icons')
+local clickable_container = require('widget.material.clickable-container')
+
+local function create_button(icon)
+    local button = clickable_container(
+        {
+            widget = wibox.widget.imagebox,
+            image = icons[icon]
+        },
+        {
+            shape = gears.shape.circle
+        }
+    )
+    return button
+end
+return create_button
