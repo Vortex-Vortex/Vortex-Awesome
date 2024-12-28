f688ab8f0a12c6cc45420cd259b902753af6c4ac
Vortex-Vortex <paulo.g.mamani@gmail.com>
2024-12-28 14:44:38 -0300

    Add logs & Fix visible property & Fix Page text & Add Delete function

--- widget/notification-center.lua
+++ widget/notification-center.lua
@@ -8,6 +8,12 @@ local icons     = require('theme.icons')
 local button_widget = require('widget.material.button-widget')
 local clickable_container = require('widget.material.clickable-container')
 
+local function log(...)
+    local timestamp = os.date("%Y/%m/%d - %H:%M:%S")
+    local args = table.concat({...}, " ")
+    gears.debug.dump(timestamp .. " " .. args)
+end
+
 local function Notification_center(s)
     local screen_width = s.geometry.width
     local notification_queue = {}
@@ -15,11 +21,18 @@ local function Notification_center(s)
     local current_callback = nil
     local update_queue = true
     local notif_counter = 0
+    local all_notif_counter = 0
     local current_filter = 'All'
     local update_notification_limited_queue
     local update_notification_center
+    local delete_queue = {}
+    local delete_notifications
+    local popup_clicked_on = false
+    local notification_rows = {
+        layout = wibox.layout.fixed.vertical
+    }
 
-    gears.debug.dump('Started notification-center.lua!')
+    log('Started function Notification_center')
 
     local count_textbox = wibox.widget{
         widget = wibox.widget.textbox,
@@ -69,12 +82,24 @@ local function Notification_center(s)
             {},
             1,
             function()
-                update_notification_limited_queue()
-                update_notification_center()
-                popup.visible = not popup.visible
+                if not popup.visible or popup_clicked_on then
+                    popup.visible = not popup.visible
+                end
+                popup_clicked_on = popup.visible
             end
         )
     )
+    notification_center_button:connect_signal("mouse::enter", function()
+        if not popup_clicked_on then
+            update_notification_center()
+            popup.visible = true
+        end
+    end)
+    notification_center_button:connect_signal("mouse::leave", function()
+        if not popup_clicked_on then
+            popup.visible = false
+        end
+    end)
 
     notification_center_header = wibox.widget{
         widget = wibox.container.margin,
@@ -97,6 +122,7 @@ local function Notification_center(s)
     }
 
     local function create_filter_widget(filter_name)
+        log('Start function create_filter_widget | arg:', filter_name)
         filter_widget = wibox.widget{
             widget = wibox.container.margin,
             clickable_container(
@@ -123,6 +149,7 @@ local function Notification_center(s)
                 1,
                 function()
                     current_filter = filter_name
+                    log('Set var:current_filter =', current_filter)
                 end
             )
         )
@@ -148,6 +175,7 @@ local function Notification_center(s)
     local num_pages = {}
     local current_page = 1
     local function create_page_widget()
+        log('Start function create_page_widget')
         local num_pages = wibox.widget{
             widget = wibox.widget.textbox,
             text = current_page .. ' / ' .. 1,
@@ -164,6 +192,7 @@ local function Notification_center(s)
                 function()
                     current_page = current_page - 1
                     num_pages.text = current_page .. ' / ' .. 1
+                    log('Update num_pages.text = ', num_pages.text)
                 end
             )
         )
@@ -175,6 +204,7 @@ local function Notification_center(s)
                 function()
                     current_page = current_page + 1
                     num_pages.text = current_page .. ' / ' .. 1
+                    log('Update num_pages.text = ', num_pages.text)
                 end
             )
         )
@@ -202,6 +232,7 @@ local function Notification_center(s)
     end
 
     local function create_trash_widget()
+        log('Start function create_trash_widget')
         local select_checkbox = wibox.widget{
             widget = wibox.widget.checkbox,
             checked = false,
@@ -214,7 +245,21 @@ local function Notification_center(s)
                 1,
                 function()
                     select_checkbox.checked = not select_checkbox.checked
--- -- --            Select all notifications, red background
+                    if select_checkbox.checked then
+                        bg = '#ff000066'
+                    else
+                        bg = '#00000000'
+                    end
+                    gears.debug.dump(notification_rows,nil,2)
+                    for _, notification_body in ipairs(notification_rows) do
+                        notification_body.bg = bg
+                        if select_checkbox.checked then
+                            table.insert(delete_queue, notification_body.identifier)
+                        end
+                    end
+                    if not select_checkbox.checked then
+                        delete_queue = {}
+                    end
                 end
             )
         )
@@ -245,8 +290,10 @@ local function Notification_center(s)
                 1,
                 nil,
                 function()
+                    log('Clicked trash_icon')
                     select_checkbox.checked = false
--- -- --            Delete Selected Notifications
+                    delete_notifications()
+                    update_notification_center()
                 end
             )
         )
@@ -283,11 +330,8 @@ local function Notification_center(s)
         forced_height = 40
     }
 
-    local notification_rows = {
-        layout = wibox.layout.fixed.vertical
-    }
-
     function update_notification_limited_queue()
+        log('Start update_notification_limited_queue')
 -- -- --Add argument to filter selecter filter from queue
 -- -- --Revise number of notifications displayed
         for i = 1, 6 do
@@ -295,9 +339,30 @@ local function Notification_center(s)
         end
     end
 
+    function delete_notifications()
+        log('Start delete_notifications')
+        for _, d_identifier in ipairs(delete_queue) do
+            for i = #notification_queue, 1, -1 do
+                if notification_queue[i].identifier == d_identifier then
+                    log('Remove notification identifier', d_identifier)
+                    table.remove(notification_queue, i)
+                    update_queue = true
+                    break
+                end
+            end
+        end
+        delete_queue = {}
+        gears.debug.dump(notification_queue)
+    end
+
     function update_notification_center()
         if not update_queue then
             return
+        end
+        log('Start update_notification_center')
+        update_notification_limited_queue()
+        for i = 0, #notification_rows do
+            notification_rows[i] = nil
         end
         for index, notification in ipairs(notification_limited_queue) do
 -- -- --Substitute 'time' for 'time ago'
@@ -324,36 +389,33 @@ local function Notification_center(s)
                 forced_width = 60
             }
 
-            local notification_body = clickable_container(
-                {
-                    layout = wibox.layout.fixed.horizontal,
+            local notification_body = wibox.widget{
+                widget = wibox.container.background,
+                clickable_container(
                     {
-                        widget = wibox.container.background,
-                        {
-                            widget = wibox.container.place,
-                            {
-                                layout = wibox.layout.fixed.vertical,
-                                {
-                                    widget = wibox.widget.imagebox,
-                                    image = notification.icon,
-                                    forced_height = 60,
-                                    forced_width = 60
-                                },
-                                notification_time
-                            },
-                            valign = 'center'
-                        },
-                        shape = gears.shape.rectangle,
-                        shape_border_width = beautiful.border_width_reduced,
-                        shape_border_color = beautiful.border_focus
-                    },
-                    {
-                        layout = wibox.layout.fixed.vertical,
+                        layout = wibox.layout.fixed.horizontal,
                         {
                             widget = wibox.container.background,
+                            {
+                                widget = wibox.container.place,
+                                {
+                                    layout = wibox.layout.fixed.vertical,
+                                    {
+                                        widget = wibox.widget.imagebox,
+                                        image = notification.icon,
+                                        forced_height = 60,
+                                        forced_width = 60
+                                    },
+                                    notification_time
+                                },
+                                valign = 'center'
+                            },
                             shape = gears.shape.rectangle,
                             shape_border_width = beautiful.border_width_reduced,
-                            shape_border_color = beautiful.border_normal,
+                            shape_border_color = beautiful.border_focus
+                        },
+                        {
+                            layout = wibox.layout.fixed.vertical,
                             {
                                 widget = wibox.widget.textbox,
                                 text = notification.title,
@@ -362,18 +424,49 @@ local function Notification_center(s)
                                 valign = 'center',
                                 forced_height = 20,
                                 forced_width = 340
-                            }
-                        },
-                        notification_text
+                            },
+                            notification_text
+                        }
+                    },
+                    {
+                        border_width = beautiful.border_width_reduced,
+                        border_color = beautiful.border_normal
                     }
-                },
-                {
-                    border_width = beautiful.border_width_reduced,
-                    border_color = beautiful.border_normal
-                }
+                ),
+                bg = '#00000000',
+                identifier = notification.identifier
+            }
+            notification_body:buttons(
+                gears.table.join(
+                    awful.button(
+                        {},
+                        3,
+                        function()
+                            log('Add notification identifier', notification.identifier, 'to delete_queue')
+                            table.insert(delete_queue, notification.identifier)
+                            notification_body.bg = '#ff000066'
+                            gears.debug.dump(delete_queue)
+                        end
+                    ),
+                    awful.button(
+                        {},
+                        4,
+                        function()
+                            notification_text.forced_height = 60
+                        end
+                    ),
+                    awful.button(
+                        {},
+                        5,
+                        function()
+                            notification_text.forced_height = notification_text:get_height_for_width(340)
+                        end
+                    )
+                )
             )
 -- -- --    Notification body signals: red delete/scroll expand/change border hover
             notification_rows[index] = notification_body
+            log('Created notification object identified by', notification.identifier)
         end
         popup:setup(
             {
@@ -389,18 +482,22 @@ local function Notification_center(s)
     function naughty.config.notify_callback(args)
         update_queue = true
         args.time = os.date("%H:%M:%S")
+        all_notif_counter = all_notif_counter + 1
 -- -- --Add args.filter according to args.title
         table.insert(notification_queue, 1, {
             title = args.title or args.appname,
             text = args.text or "",
             icon = args.icon or icons.notification_icon,
 -- -- --Add icon substitution for icon=file:///tmp...
-            time = args.time
+            time = args.time,
+            identifier = all_notif_counter
         })
-
+        if popup.visible then
+            update_notification_center()
+        end
         notif_counter = notif_counter + 1
         count_textbox.text = notif_counter
-
+        log('Received notification identified by', all_notif_counter)
         return args
     end
 
