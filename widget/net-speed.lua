ef80f6a74d91e683584dd37c59003bd872611597
Vortex-Vortex <paulo.g.mamani@gmail.com>
2025-01-31 21:10:49 -0300

    New widget model, uses less space

--- widget/net-speed.lua
+++ widget/net-speed.lua
@@ -5,6 +5,12 @@ local icons = require('theme.icons')
 
 local function Net_speed()
     local interface = 'eno1'
+    awful.spawn.easy_async_with_shell(
+        [[ip route | grep default | head -n1 | awk '{print $5}']],
+        function(stdout)
+            interface = string.gsub(stdout, '\n', '')
+        end
+    )
 
     local function make_readable(bytes)
         bits = 8 * bytes
@@ -24,33 +30,26 @@ local function Net_speed()
         return math.floor(speed + 0.5) .. dim
     end
 
-    local rx_textbox = wibox.widget{
-        widget = wibox.widget.textbox,
-        text = '0b/s',
-        font = beautiful.system_font .. 'bold 10',
-        align = 'left',
-        forced_width = 55
+    local x_image = wibox.widget{
+        widget = wibox.widget.imagebox,
+        image = icons.arrow_up
     }
-    local tx_textbox = wibox.widget{
+    local x_textbox = wibox.widget{
         widget = wibox.widget.textbox,
         text = '0b/s',
         font = beautiful.system_font .. 'bold 10',
-        align = 'left',
-        forced_width = 55
+        align = 'right',
+        forced_width = 60
     }
 
     local net_speed_widget = wibox.widget{
         layout = wibox.layout.fixed.horizontal,
+        x_image,
         {
-            widget = wibox.widget.imagebox,
-            image = icons.arrow_down
-        },
-        rx_textbox,
-        {
-            widget = wibox.widget.imagebox,
-            image = icons.arrow_up
-        },
-        tx_textbox
+            widget = wibox.container.margin,
+            x_textbox,
+            right = 10
+        }
     }
 
     local prev_rx = 0
@@ -65,8 +64,13 @@ local function Net_speed()
                 local speed_rx = cur_rx - prev_rx
                 local speed_tx = cur_tx - prev_tx
 
-                rx_textbox.text = tostring(make_readable(speed_rx))
-                tx_textbox.text = tostring(make_readable(speed_tx))
+                if speed_rx > speed_tx then
+                    x_textbox.text = tostring(make_readable(speed_rx))
+                    x_image.image = icons.arrow_down
+                else
+                    x_textbox.text = tostring(make_readable(speed_tx))
+                    x_image.image = icons.arrow_up
+                end
 
                 prev_rx = cur_rx
                 prev_tx = cur_tx
