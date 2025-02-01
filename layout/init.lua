200c4630c7ab0bc44b283b4da9292eed87e9aff4
Vortex-Vortex <paulo.g.mamani@gmail.com>
2025-01-31 21:53:06 -0300

    Add top panel visibility change on client mode

--- layout/init.lua
+++ layout/init.lua
@@ -6,3 +6,37 @@ awful.screen.connect_for_each_screen(
         s.top_panel = top_panel(s)
     end
 )
+
+function updateBarsVisibility()
+    for s in screen do
+        if s.selected_tag then
+            local fullscreen = s.selected_tag.fullscreenMode
+            s.top_panel.visible = not fullscreen
+        end
+    end
+end
+
+_G.tag.connect_signal(
+    'property::selected',
+    function(t)
+        updateBarsVisibility()
+    end
+)
+
+_G.client.connect_signal(
+    'property::fullscreen',
+    function(c)
+        c.screen.selected_tag.fullscreenMode = c.fullscreen
+        updateBarsVisibility()
+    end
+)
+
+_G.client.connect_signal(
+    'unmanage',
+    function(c)
+        if c.fullscreen then
+            c.screen.selected_tag.fullscreenMode = false
+            updateBarsVisibility()
+        end
+    end
+)
