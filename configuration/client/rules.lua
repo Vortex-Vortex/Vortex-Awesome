e821102c45e1842d5ae3fa0480c26d8d0dba4f26
Vortex-Vortex <paulo.g.mamani@gmail.com>
2025-01-31 21:17:33 -0300

    Add tag rules, buttons assignment and PIP support

--- configuration/client/rules.lua
+++ configuration/client/rules.lua
@@ -1,4 +1,5 @@
 local awful = require('awful')
+local client_buttons = require('configuration.client.buttons')
 
 awful.rules.rules = {
     {
@@ -6,6 +7,7 @@ awful.rules.rules = {
         properties = {
             focus = awful.client.focus.filter,
             raise = true,
+            buttons = client_buttons,
             screen = awful.screen.preferred,
             placement = awful.placement.no_offscreen,
             floating = false,
@@ -17,5 +19,35 @@ awful.rules.rules = {
             maximized_horizontal = false,
             maximized_vertical = false
         }
-    }
+    },
+    {
+        rule = {
+            class = "TelegramDesktop"
+        },
+        properties = {
+            tag = "3"
+        }
+    },
+    {
+        rule = {
+            role = "spawn_on_2"
+        },
+        properties = {
+            tag = "2"
+        }
+    },
+    {
+        rule_any = {
+            name = { 'Picture-in-Picture' },
+            type = { 'dialog' },
+            class = { 'Lxpolkit', 'lxpolkit', 'mediainfo-gui', 'Mediainfo-gui', 'mpv', 'kcalc', 'kruler', 'huiontablet', 'shotwell', 'Shotwell', 'feh', 'mupdf', 'MuPDF' }
+        },
+        properties = {
+            floating = true,
+            opacity = 1,
+            ontop = true,
+            skip_decoration = true,
+            placement = awful.placement.centered
+        }
+    },
 }
